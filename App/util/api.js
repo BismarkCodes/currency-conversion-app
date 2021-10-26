d124caf2e8d98cd3ad0d9be9884f68361c600459
bismarkamanor <bismarkokleteyamanor@gmail.com>
2021-10-26 22:02:24 +0000

    Updated the API file

--- App/util/api.js
+++ App/util/api.js
@@ -1,20 +1,22 @@
 import { format } from "date-fns";
 import { RATES } from "./conversionRates";
 
+// creating custom api
 export const api = (_path = "") => {
-  // getting specific path from url
+  // getting the path from the url
   const [path] = _path.split("?");
-  if (path.length === 0) {
-    return Promise.reject(new Error("Path required"));
+  // checking if url exists
+  if (_path.length === 0) {
+    return Promise.reject(new Error("URL required"));
   }
+  // checking if path is correct
   if (path !== "/latest") {
-    return Promise.reject(new Error("Invalid path specified"));
+    return Promise.reject(new Error("Invalid url"));
   }
+  // get base currency from url
+  const baseCurrency = _path.split("base=")[1]; // get the key or basecurrency
 
-  // getting value or parameter from url
-  const baseCurrency = _path.split("base=")[1]; // i.e ['/latest?base=', USD]
-
-  //   adding delay to api response
+  // setting delay on api response
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
@@ -25,6 +27,6 @@ export const api = (_path = "") => {
           [baseCurrency]: 1,
         },
       });
-    }, 800);
+    }, 500);
   });
 };
